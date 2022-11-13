cfeaa7f2f429fa768f38fca722c79a00e5171186
Charles Essien <charlespheonix@gmail.com>
2022-11-13 07:09:02 +0100

    renamed request controller endpoints

--- src/main/java/com/tagalong/controller/RequestController.java
+++ src/main/java/com/tagalong/controller/RequestController.java
@@ -15,21 +15,22 @@ import java.util.Optional;
 public class RequestController {
     private final RequestRepository requestRepository;
 
-    @PutMapping
+    @PutMapping(value = "/create-request")
     @PostMapping
     public ResponseEntity<Request> create(@RequestBody Request request, @RequestHeader("Authorization") String Authorization) {
 
         return ResponseEntity.ok(this.requestRepository.save(request));
     }
 
-    @GetMapping
+
+    @GetMapping(value = "/get-all-requests")
     public ResponseEntity<List<Request>> getAllRequest(@RequestHeader("Authorization") String Authorization) {
 
         return ResponseEntity.ok(this.requestRepository.findAll());
     }
 
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/get-request/{id}")
     public ResponseEntity<Request> getRequest(@PathVariable(name = "id") Long id, @RequestHeader("Authorization") String Authorization) {
         Optional<Request> request = this.requestRepository.findById(id);
         if (request.isPresent()) {
